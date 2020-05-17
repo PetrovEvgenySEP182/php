553bf8ca1d66438f1c6aaf78903b5119596374d8
Petrov Evgeny <65103867+PetrovEvgenySEP182@users.noreply.github.com>
2020-05-17 18:51:14 +0600

    Add files via upload

---
+++ Lesson10/app/HTML/Tag/TagBody.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HTML\Tag;
+
+class TagBody
+{
+    protected $body;
+
+    public function __construct()
+    {
+        $this->clear();
+    }
+
+    function clear(){
+        $this->body = [];
+    }
+
+    function append($value){
+        $this->body[] = $value;
+    }
+
+    function prepend($value){
+        array_unshift($this->body, $value);
+    }
+
+    function __toString()
+    {
+        return implode($this->body);
+    }
+}
